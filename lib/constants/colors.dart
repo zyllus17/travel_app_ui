c1d9dbe4a4e492c4dc1273e8a7f1ce577cb13059
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-01-26 17:02:30 +0530

    Colors used in the app

---
+++ lib/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  static const Color black = Color(0xFF000000);
+  static const Color lightGrey = Color(0xFF878593);
+  static const Color purple = Color(0xFF5967BF);
+  static const Color circleGrey = Color(0xFFC4C4C4);
+  static const Color textGrey = Color(0xFF959595);
+  static const Color bottomNav = Color(0xFFD8DDFD);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color starYellow = Color(0xFFEECB4F);
+}
