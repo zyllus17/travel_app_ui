4164bf13c5add9e6fdc23fc4c4525b828c4d121c
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-01-26 17:03:22 +0530

    Text used in the app

---
+++ lib/constants/text.dart
@@ -0,0 +1,28 @@
+class AppText {
+  AppText._();
+
+  static const String trips = "Trips";
+  static const String mountain = "Mountain";
+  static const String discover = "Discover";
+  static const String places = "Places";
+  static const String inspiration = "Inspiration";
+  static const String emotions = "Emotions";
+  static const String exploreMore = "Explore More";
+  static const String seeAll = "See all";
+  static const String hiking = "Hiking";
+  static const String kayaking = "Kayaking";
+  static const String balloning = "Balloning";
+  static const String snorkling = "Snorkling";
+  static const String himalaya = "Himalaya";
+  static const String price = "\$ 1500";
+  static const String himachal = "Himachal Pradesh, India";
+  static const String fiveStar = "(5.0)";
+  static const String people = "People";
+  static const String peopleGroup = "No. of people in your group";
+  static const String description = "Description";
+  static const String bookTrip = "Book Trip Now";
+  static const String himalayaDescription =
+      "The Himalayas, or Himalaya, are a mountain range in Asia separating the plains of the Indian subcontinent from the Tibetan Plateau.";
+  static const String hikeDetails =
+      "Mountain hike gives you an incredible sense of freedom along with a lot of endurance";
+}
