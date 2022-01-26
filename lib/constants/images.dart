2dbd3b914a846670e91a6ecd93330ba433a0406f
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-01-26 17:02:43 +0530

    Images used in the app

---
+++ lib/constants/images.dart
@@ -0,0 +1,15 @@
+class AppImages {
+  AppImages._();
+
+  static const String balloning = "assets/balloning.png";
+  static const String denali = "assets/denali.png";
+  static const String hiking = "assets/hiking.png";
+  static const String himalaya = "assets/himalaya.png";
+  static const String kayaking = "assets/kayaking.png";
+  static const String lhotse = "assets/lhotse.png";
+  static const String orientationOne = "assets/orientation_one.png";
+  static const String orientationTwo = "assets/orientation_two.png";
+  static const String orientationThree = "assets/orientation_three.png";
+  static const String profile = "assets/profile.png";
+  static const String snorkling = "assets/snorkling.png";
+}
